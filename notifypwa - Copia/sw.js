6cce33d7528180cf57ab144b514eebdc1f178aa6
henriiqsantzz <henriqsantz@gmail.com>
2025-10-03 17:27:12 -0300

    Add files via upload

---
+++ notifypwa - Copia/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('notifylab-v1').then(cache => cache.addAll([
+    './', './index.html', './main.js', './manifest.json',
+    './icons/icon-192.png', './icons/icon-512.png', './icons/badge.png',
+    './icons/swatch-c6.png', './icons/swatch-e.png', './icons/swatch-b.png', './icons/swatch-f.png',
+    './icons/swatch-kiwify.png'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil((async () => {
+    const all = await clients.matchAll({ includeUncontrolled: true, type: 'window' });
+    if (all && all.length) all[0].focus(); else clients.openWindow('/');
+  })());
+});
